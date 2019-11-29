8af8128aabd9a1af73ace56999834c49957c42ce
JiKon <canyounotspammethanks@gmail.com>
2019-11-29 14:20:25 +0100

    Circle opdracht

---
+++ 02_circles/script.js
@@ -0,0 +1,39 @@
+// maak een variabele voor de canvas
+const canvas = document.getElementById('canvas');
+
+// leg een 2d-context over de canvas
+const context = canvas.getContext('2d');
+
+//maak de canvas schermvullend
+const width = window.innerWidth;
+const height = window.innerHeight;
+canvas.width = width;
+canvas.height = height;
+
+// hier kun je op de canvas tekenen
+
+// -------------------------------------------------------
+
+let colors = ["red","green","blue","orange","yellow","purple"]
+
+console.log(getRandomNumber(200));
+
+function getRandomNumber(max){
+  return Math.floor(Math.random()*max);
+}
+
+
+function drawCircle(){
+  context.beginPath()
+  context.fillStyle = colors[getRandomNumber(colors.length)];
+  context.arc(getRandomNumber(width),getRandomNumber(height),20,0,Math.PI*2);
+  context.stroke();
+  context.fill();
+  context.closePath();
+}
+
+function animate(){
+  drawCircle();
+}
+
+setInterval(animate,100);
